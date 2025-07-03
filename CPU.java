97565084a1196cd4a1027ead7da8786dbfce71b0
javaPaulVI <167591676+javaPaulVI@users.noreply.github.com>
2025-07-03 14:33:46 +0200

    Changes CPU.java

--- CPU.java
+++ CPU.java
@@ -2,17 +2,23 @@
 
 public class CPU{
 
-	public Iterator<Result<?> run(Generator generator){
+	public CPU(){
+
+	}
+	/** Calls step() for each tick and gives the program instructions as parameters
+	 * @return 
+	 */
+	public Iterator<Result<?> run(){
 		return iterator(y -> {
-        	for (instruction : program.getInstructions(){
+        	for (instruction : program.getInstructions()){
 			Result<> result = step(instruction[0], instruction[1], instruction[2], instruction[3]);
 			y.yield(result)
-		});
+		}});
 
 	}
 	
-	private Result<?> step(byte opCode, byte arg1, byte arg2, byte tick_or_address){
-		// Check for callstack
+	p Result<?> step(byte opCode, byte arg1, byte arg2, byte tick_or_address){
+		// Check for callstack (implement)
 
 		byte input1 = 0;
 		byte input2 = 0;
@@ -21,11 +27,24 @@ public class CPU{
 
 		if (opCodeArray[7].get() == 1){
 			input1 = arg1;
-		} else {
-			if({0,1,2,3,4,5}.contain(arg1){
+			opCodeArray[7] = new Bit(0)
+		}  else {
+			if({0,1,2,3,4,5}.contain((int)arg2)){
 				input1 = registers[arg1].getValue();
 			}
-
+			switch((int) opCode){
+				case 6:
+					input1 = counter.getValue();
+					break;
+				case 7:
+					input1 = in.next()
+				case 8:
+					ram.setIndex(registers[4].getValue());
+					input1 = ram.load();
+					break;
+				case 9:
+					input1=stack.pop()
+			}
 		}
 		
 
@@ -33,7 +52,7 @@ public class CPU{
 			input2 = arg2;
 		
 		} else {
-			if({0,1,2,3,4,5}.contain((int)arg2){
+			if({0,1,2,3,4,5}.contain((int)arg2)){
 				input2 = registers[arg2].getValue();
 			}
 			switch((int) opCode){
@@ -62,3 +81,4 @@ public class CPU{
 			
 	}
 }
+

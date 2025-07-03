5d69c6c04a1b815f7b4e479eeb8f54fc2b94cf4d
javaPaulVI <167591676+javaPaulVI@users.noreply.github.com>
2025-07-03 14:00:15 +0200

    Update CPU.java

--- CPU.java
+++ CPU.java
@@ -2,11 +2,13 @@
 
 public class CPU{
 
-	public Generator run(Generator generator){
-		
-		for (instruction : program.getInstructions()){
-			generator.add(step())	
-		}
+	public Iterator<Result<?> run(Generator generator){
+		return iterator(y -> {
+        	for (instruction : program.getInstructions(){
+			Result<> result = step(instruction[0], instruction[1], instruction[2], instruction[3]);
+			y.yield(result)
+		});
+
 	}
 	
 	private Result<?> step(byte opCode, byte arg1, byte arg2, byte tick_or_address){
@@ -21,7 +23,7 @@ public class CPU{
 			input1 = arg1;
 		} else {
 			if({0,1,2,3,4,5}.contain(arg1){
-				input1 = registers[1].getValue();
+				input1 = registers[arg1].getValue();
 			}
 
 		}
@@ -31,9 +33,20 @@ public class CPU{
 			input2 = arg2;
 		
 		} else {
-			if({0,1,2,3,4,5}.contain(arg2){
-				input2 = registers[1].getValue();
-			}		
+			if({0,1,2,3,4,5}.contain((int)arg2){
+				input2 = registers[arg2].getValue();
+			}
+			switch((int) opCode){
+				case 6:
+					input2 = counter.getValue();
+					break;
+				case 7:
+					input2 = in.next()
+				case 8:
+					ram.setIndex(registers[4].getValue());
+					input2 = ram.load();
+					break;
+			}
 		}
 
 		if (opCodeArray[6].get() == 1){
@@ -42,6 +55,7 @@ public class CPU{
 				counter.setValue(tick_or_adress)
 				return Ok<>(true)
 			}
+			return Ok<>(false)
 		}
 		
 		
